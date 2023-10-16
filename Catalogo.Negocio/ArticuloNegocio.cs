1709f8a57144096b485a1402ad3d48e20e23ab2f
JuaneloDev <juancromero91@outlook.com>
2023-10-15 23:23:08 -0300

    Cambio consulta SQL
    Cambio consulta para que no traiga registros repetidos

--- Catalogo.Negocio/ArticuloNegocio.cs
+++ Catalogo.Negocio/ArticuloNegocio.cs
@@ -427,9 +427,9 @@ namespace Catalogo.Negocio
 
             try
             {
-                string consulta = "SELECT A.Id, A.Codigo, A.Nombre, A.Descripcion , M.Descripcion as Marca, C.Descripcion as Categoria, I.ImagenUrl, I.Id AS IdImagen, A.Precio, A.IdCategoria, A.IdMarca FROM ARTICULOS A LEFT JOIN CATEGORIAS C ON A.IdCategoria = C.Id LEFT JOIN MARCAS M ON A.IdMarca = M.Id LEFT JOIN IMAGENES I ON A.Id = I.IdArticulo WHERE ";
+                string consulta = "SELECT TOP 1 A.Id, A.Codigo, A.Nombre, A.Descripcion , M.Descripcion as Marca, C.Descripcion as Categoria, I.ImagenUrl, I.Id AS IdImagen, A.Precio, A.IdCategoria, A.IdMarca FROM ARTICULOS A LEFT JOIN CATEGORIAS C ON A.IdCategoria = C.Id LEFT JOIN MARCAS M ON A.IdMarca = M.Id LEFT JOIN IMAGENES I ON A.Id = I.IdArticulo WHERE ";
 
-                consulta += "A.Id LIKE '" + id + "%'";
+                consulta += "A.Id =" + id;
 
                 datos.SetConsulta(consulta);
                 datos.EjecutarLectura();
